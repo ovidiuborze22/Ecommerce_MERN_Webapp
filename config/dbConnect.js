c106d9e9d5c297f6ecf8aa48c4dc27fc9eec156a
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-11-05 18:01:52 +0000

    created database connection with MongoDB

---
+++ config/dbConnect.js
@@ -0,0 +1,13 @@
+// dbConnect.js
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+    try {
+        const conn = mongoose.connect(process.env.DB_URI);
+        console.log("Database Connected Successfully");
+    } catch (error) {
+        console.error("Database connection error", error);
+    }
+};
+
+module.exports = dbConnect;
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+// index.js
 const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const dotenv = require("dotenv").config();
 const app = express();
-const dotenv = require("dotenv");
 const PORT = process.env.PORT || 5000;
+
+dbConnect();
 
 app.use("/", (req, res) => {
     res.send("Hello from server side");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at PORT ${PORT}`);
+app.listen(PORT, (error) => {
+    if (error) {
+        console.error(`Failed to start the server: ${error}`);
+    } else {
+        console.log(`Server is running at PORT ${PORT}`);
+    }
 });
